0059b59a5a2376b644229c9b83ea596a0e1fb6aa
Memory Collection <csszue@gmail.com>
2023-07-23 10:00:51 +0800

    Update he-simple-common.js

--- js/he-simple-common.js
+++ js/he-simple-common.js
@@ -3,7 +3,7 @@
   c.rel = 'stylesheet'
   c.href = 'https://sou.36g.top/css/he-simple.css?v=1.4.0'
   var s = d.createElement('script')
-  s.src = 'https://sou.36g.top/css/he-simple.js?v=1.4.0'
+  s.src = 'https://sou.36g.top/js/he-simple.js?v=1.4.0'
   var sn = d.getElementsByTagName('script')[0]
   sn.parentNode.insertBefore(c, sn)
   sn.parentNode.insertBefore(s, sn)
