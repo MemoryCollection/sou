4791ecfb316d5079daf7e1d21d8c78e32a35625d
MemoryCollection <csszue@gmail.com>
2024-03-23 10:26:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## 个人导航站
+图标基于[iconfont](https://www.iconfont.cn/)   
+
+基于  [5iux.github.io](https://github.com/5iux/5iux.github.io)  修改
+
+
+sou.wi6.cn
+
+
+     
+
+    
+
+
---
+++ js/he-simple-common.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://sou.5uo.top/css/he-simple.css?v=1.4.0'
+  var s = d.createElement('script')
+  s.src = 'https://sou.5uo.top/js/he-simple.js?v=1.4.0'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
